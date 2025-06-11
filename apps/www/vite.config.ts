7b01ee394b6693bc94fecbea3221621bf6764ee8
Barsnes <tobias.barsnes@digdir.no>
2025-06-11 13:53:16 +0200

    disable netlify and enable prerender

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       plugins: [],
     },
   },
-  plugins: [tsconfigPaths(), envOnlyMacros(), reactRouter(), netlifyPlugin()],
+  plugins: [tsconfigPaths(), envOnlyMacros(), reactRouter(), /* netlifyPlugin() */],
 });
